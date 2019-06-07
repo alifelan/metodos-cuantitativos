75ba19a8ae27c441d50d3ed6d45578ea326e8773
Ali <alifelanv@gmail.com>
2019-06-06 19:41:02 -0500

    Add docstring

--- read_data.py
+++ read_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def read(filename):
+    """Reads the file received in the parameter. File must be a zip."""
     df = pd.read_csv(filename, compression='zip')
     df = df.loc[:, ~df.columns.str.match('Unnamed')]
     return df
