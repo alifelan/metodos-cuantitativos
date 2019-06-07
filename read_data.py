46f351aeef23fa9bb07e65277850a979fae78f0f
Ali <alifelanv@gmail.com>
2019-06-07 00:00:50 -0500

    Add default name and parse datetime columns

--- read_data.py
+++ read_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-def read(filename):
+def read(filename='result.zip'):
     """Reads the file received in the parameter. File must be a zip."""
-    df = pd.read_csv(filename, compression='zip')
-    df = df.loc[:, ~df.columns.str.match('Unnamed')]
+    df = pd.read_csv(filename, compression='zip', low_memory=False)
+    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+    df['Time'] = pd.to_datetime(df['Time'], format='%H:%M')
     return df
