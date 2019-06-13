47ba246b0e940d8903bd0e32df6455b2fdeb6458
Ali <alifelanv@gmail.com>
2019-06-12 19:27:32 -0500

    Fix typos, add test method for each normality test, and update methods to return results

--- normality.py
+++ normality.py
@@ -4,22 +4,47 @@ import pandas as pd
 
 def test(df: pd.DataFrame):
     """Test each column with the methods."""
+    print('Testing normality...')
     for column in df:
-        anderson(df[column])
-        kstest_test(df[column])
-        shapiro_test(df[column])
+        print(anderson_test(df[column]))
+        print(kstest_test(df[column]))
+        print(shapiro_test(df[column]))
+
+
+def test_and(df: pd.DataFrame):
+    """Test each column with anderson test."""
+    print('Running Anderson-Darling test...')
+    for column in df:
+        print(anderson(df[column]))
+        input("Press enter to continue.")
+
+
+def test_ks(df: pd.DataFrame):
+    """Test each column with ks test."""
+    print('Running Kolmogorov-Smirnov test...')
+    for column in df:
+        print(kstest_test(df[column]))
+        input("Press enter to continue.")
+
+
+def test_sh(df: pd.DataFrame):
+    """Test each column with shapiro test."""
+    print('Running Shapiro-Wilk test...')
+    for column in df:
+        print(shapiro_test(df[column]))
+        input("Press enter to continue.")
 
 
 def anderson_test(data: pd.Series):
     """Run Anderson-Darling test on data received"""
-    anderson(data)
+    return anderson(data)
 
 
 def kstest_test(data: pd.Series):
     """Run Kolmogorov-Smirnov test on data received"""
-    kstest(data, 'norm')
+    return kstest(data, 'norm')
 
 
 def shapiro_test(data: pd.Series):
     """Run Ryan-Joiner equivalent, Shapiro-Wilk test on data received"""
-    shapiro(data)
+    return shapiro(data)
