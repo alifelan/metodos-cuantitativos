219e7f342f3112151db01b28d724f3269181dce3
Ali <alifelanv@gmail.com>
2019-06-12 19:25:02 -0500

    Add filter_df method

--- data.py
+++ data.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 
+def filter_df(df: pd.DataFrame = pd.DataFrame()):
+    if df.empty:
+        print('Reading data...')
+        df = read()
+    df2 = df.iloc[:, 3:5]
+    df2['Speed_limit'] = df['Speed_limit']
+    return df2
+
+
 def read(filename='result.zip'):
     """Reads the file received in the parameter. File must be a zip."""
     df = pd.read_csv(filename, compression='zip', low_memory=False)
