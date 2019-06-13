ed9b2b5c7a2534a46712a6593138a4690596815e
Ali <alifelanv@gmail.com>
2019-06-12 19:26:08 -0500

    Fix typos, add test method for each distribution, and update methods to return results

--- distribution.py
+++ distribution.py
@@ -3,22 +3,62 @@ from numpy.random import exponential, gamma, weibull, lognormal, pareto
 
 def test(df):
     """Run each distribution on each column."""
-    for colummn in df:
+    for column in df:
         exp(df[column])
-        gam(df[colummn])
-        wei(df[colummn])
-        logn(df[colummn])
+        gam(df[column])
+        wei(df[column])
+        logn(df[column])
         pare(df[column])
+
+
+def test_exp(df):
+    """Run exponential distribution on each column."""
+    print('Running Exponential test...')
+    for column in df:
+        print(exp(df[column]))
+        input("Press enter to continue.")
+
+
+def test_gam(df):
+    """Run gamma distribution on each column."""
+    print('Running Gamma test...')
+    for column in df:
+        print(gam(df[column]))
+        input("Press enter to continue.")
+
+
+def test_wei(df):
+    """Run wei distribution on each column."""
+    print('Running Weibull test...')
+    for column in df:
+        print(wei(df[column]))
+        input("Press enter to continue.")
+
+
+def test_logn(df):
+    """Run logn distribution on each column."""
+    print('Running Lognormal test...')
+    for column in df:
+        print(logn(df[column]))
+        input("Press enter to continue.")
+
+
+def test_pare(df):
+    """Run pare distribution on each column."""
+    print('Running Pareto test...')
+    for column in df:
+        print(pare(df[column]))
+        input("Press enter to continue.")
 
 
 def exp(df):
     """Exponential distribution."""
-    exponential(df)
+    return exponential(df)
 
 
 def gam(df):
     """Gamma distribution."""
-    gamma(df)
+    return gamma(df)
 
 
 def wei(df):
