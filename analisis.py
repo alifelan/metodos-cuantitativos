987e0698db84037e4571bf666dbce569efcd3ff4
martinrrm <martin.ruiz.mares@gmail.com>
2019-06-13 20:49:28 -0500

    Addes analisis file and images

---
+++ analisis.py
@@ -0,0 +1,234 @@
+import numpy as np 
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sb
+from data import * 
+
+def stats_location_easting(data):
+    """ Analisys of the variable Location Easting OSGR """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Location Easting OSGR")
+    plt.show() 
+
+def stats_location_northing(data):
+    """ Analisys of the variable Location Northing OSGR """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Location Northing OSGR")
+    plt.show()
+
+def date(data):
+    """ Analisys of the variable Date """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Date")
+    plt.show()
+
+def day_week(data):
+    """ Analisys of the variable Day Week """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Day Week")
+    plt.show()
+
+def time(data):
+    """ Analisys of the variable Time """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Time")
+    plt.show()
+
+def speed_limit(data):
+    """ Analisys of the variable Speed Limit"""
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Speed Limit")
+    plt.show()
+
+def light_conditions(data):
+    """ Analisys of the variable Light Conditions """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Light Conditions")
+    plt.show()
+
+def first_road_class(data):
+    """ Analisys of the variable First Road Class """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of First Road Class")
+    plt.show()
+
+def second_road_class(data):
+    """ Analisys of the variable Second Road Class """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Second Road Class")
+    plt.show()
+
+def first_road_number(data):
+    """ Analisys of the variable First Road Number """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of First Road Number ")
+    plt.show()
+
+def second_road_number(data):
+    """ Analisys of the variable Second Road Number """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Second Road Number")
+    plt.show()
+
+def weather(data):
+    """ Analisys of the variable Weather """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Weather")
+    plt.show()
+
+def area(data):
+    """ Analisys of the variable Area """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Area")
+    plt.show()
+
+def year(data):
+    """ Analisys of the variable Year """
+    media = np.mean(data)
+    varianza = np.var(data)
+    desviacion = np.std(data)
+    simetria = data.skew()
+    curtosis = data.kurtosis()
+    print(media)
+    print(varianza)
+    print(simetria)
+    print(curtosis)
+    plt.hist(data, bins='auto')
+    plt.title("Histogram of Year")
+    plt.show()
+
+
+def init():
+    data = read()
+    stats_location_easting(data['Location_Easting_OSGR'])
+    stats_location_northing(data['Location_Northing_OSGR'])
+    #data(data[])
+    day_week(data['Day_of_Week'])
+    #time(data[])
+    speed_limit(data['Speed_limit'])
+    #ilight_conditions(data[])
+    first_road_class(data['1st_Road_Class'])
+    second_road_class(data['2nd_Road_Class'])
+    first_road_number(data['1st_Road_Number'])
+    second_road_number(data['2nd_Road_Number'])
+    #weather(data[])
+    area(data['Urban_or_Rural_Area'])
+    year(data['Year'])
