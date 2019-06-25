cab205ccd3fbebfb1b77c376b7342e6d73cfdb23
Ali <alifelanv@gmail.com>
2019-06-25 12:34:13 -0500

    Remove unnecessary columns and method

--- data.py
+++ data.py
@@ -15,24 +15,8 @@ def filter(df: pd.DataFrame = pd.DataFrame()):
     df2['Day'] = df['Date'].dt.strftime('%d').astype(int)
     df2['Day_of_Week'] = df['Day_of_Week']
     df2['Time'] = np.array([t.timestamp() for t in df['Time']]) - df['Time'].min().timestamp()
-    df2['Speed_limit'] = df['Speed_limit']
-    df2['Light_Conditions'] = df['Light_Conditions']
-    df2['1st_Road_Class'] = df['1st_Road_Class']
-    df2['2nd_Road_Class'] = df['2nd_Road_Class']
-    df2['1st_Road_Number'] = df['1st_Road_Number']
-    df2['2nd_Road_Number'] = df['2nd_Road_Number']
     df2['Weather_Conditions'] = df['Weather_Conditions']
-    df2['Urban_or_Rural_Area'] = df['Urban_or_Rural_Area']
     return pd.get_dummies(df2)
-
-
-def filter_numeric(df: pd.DataFrame):
-    """Filters DataFrame to use only the numerical columns"""
-    df2 = pd.DataFrame()
-    df2['Location_Easting_OSGR'] = df['Location_Easting_OSGR']
-    df2['Location_Northing_OSGR'] = df['Location_Northing_OSGR']
-    df2['Speed_limit'] = df['Speed_limit']
-    return df2
 
 
 def read(filename='result.zip'):
