a8d6576bdaeb70d9254ff04734c85b0fbe7cc2bd
Ali <alifelanv@gmail.com>
2019-06-25 13:34:33 -0500

    Add filter location method, and read dataframes with pickle

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pickle
 
 
 def filter(df: pd.DataFrame = pd.DataFrame()):
@@ -19,7 +20,30 @@ def filter(df: pd.DataFrame = pd.DataFrame()):
     return pd.get_dummies(df2)
 
 
-def read(filename='result.zip'):
+def filter_location(df: pd.DataFrame = pd.DataFrame()):
+    """Returns location columns"""
+    if df.empty:
+        df = read()
+    df = df.dropna()
+    df2 = pd.DataFrame()
+    df2['Location_Easting_OSGR'] = df['Location_Easting_OSGR']
+    df2['Location_Northing_OSGR'] = df['Location_Northing_OSGR']
+    return df2
+
+
+def read():
+    with open('./filtered.pkl', 'rb') as file:
+        df = pickle.load(file)
+    return df
+
+
+def read_location():
+    with open('./locations.pkl', 'rb') as file:
+        df = pickle.load(file)
+    return df
+
+
+def read_csv(filename='result.zip'):
     """Reads the file received in the parameter. File must be a zip."""
     print('Reading data...')
     df = pd.read_csv(filename, compression='zip', low_memory=False)
