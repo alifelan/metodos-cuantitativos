c921da942e4f128ecca1034cd214f145c7dc0628
martinrrm <martin.ruiz.mares@gmail.com>
2019-06-26 00:02:25 -0500

    Added Random Forest

--- predict.py
+++ predict.py
@@ -1,9 +1,12 @@
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 from joblib import dump, load
 from data import read_location
 import pickle
+import numpy as np
 
 
 def train(df):
@@ -14,6 +17,19 @@ def train(df):
     pipeline.fit(df.iloc[:, 2:])
     dump(pipeline, 'pipeline.joblib')
 
+def trainRandomForest(df):
+    labels = np.array(df['Longitude'], df['Latitude'])
+    features = df.drop('Longitude', axis = 1)
+    features = df.drop('Latitude', axis = 1)
+    feature_list = list(features.columns)
+    features = np.array(features)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+    scaler = preprocessing.StandardScaler()
+    rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    #pipeline = Pipeline([('Scaler', scaler), ('RandomForestRegressor', rf)])
+    # Skip location columns
+    rf.fit(train_features, train_labels);
+    #dump(pipeline, 'pipelineRF.joblib')
 
 def predict(data):
     print('Loading model...')
